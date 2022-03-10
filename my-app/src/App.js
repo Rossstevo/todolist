98ecbab8125a3cdcf608d3881490ae215f07f8be
Ross Stevenson <Ross.w.Stevenson@gmail.com>
2022-03-10 09:59:40 +0000

    added input and checkbox features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import TodoList from './TodoList';
 
 function App() {
+{/* empty array because we have no to-do's */}
+const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false}])
+
+
+
   return (
     <div className="App">
-    
       <header className="App-header">
+      
       <h1> To do list</h1>
-      <TodoList/>
+      <TodoList todos={todos}/>
       <input type = "text"/>
       <button>Add Todo</button>
       <button>Clear list</button>
--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Todo from './Todo'
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
-    <div>test</div>
+      todos.map(todo => {
+        return <Todo key={todo.id} todo={todo}/>
+      })
+
+
+
+    
   )
 }
