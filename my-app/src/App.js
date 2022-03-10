e5995fdbc7a95df3926cf3504e1c216b13254972
Ross Stevenson <Ross.w.Stevenson@gmail.com>
2022-03-10 10:08:05 +0000

    added input handler and useRef

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './App.css';
 import TodoList from './TodoList';
 
 function App() {
 {/* empty array because we have no to-do's */}
-const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false}])
+const [todos, setTodos] = useState([])
+const todoNameRef = useRef();
 
-
-
+function handleAddTodo(e) {
+  const name = todoNameRef.current.value
+  if (name === '') return 
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: 1, name: name, complete: false}]
+    }
+      )
+    console.log(name)
+    todoNameRef.current.value = null;
+  
+}
   return (
     <div className="App">
       <header className="App-header">
       
       <h1> To do list</h1>
       <TodoList todos={todos}/>
-      <input type = "text"/>
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type = "text"/>
+      <button onClick = {handleAddTodo}>Add Todo</button>
       <button>Clear list</button>
       <div>0 left to do </div>
 
