e2dc328107f8e9797f500bcd3886ae29253e74cd
Ross Stevenson <Ross.w.Stevenson@gmail.com>
2022-03-10 09:41:16 +0000

    created HTML

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
       <h1> To do list</h1>
+      <TodoList/>
+      <input type = "text"/>
+      <button>Add Todo</button>
+      <button>Clear list</button>
+      <div>0 left to do </div>
+
+
+
+
+
+
+
+
       </header>
     </div>
   );
