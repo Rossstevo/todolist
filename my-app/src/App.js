546f98f66bb3946d5f132abd5c7798105c817aea
Ross Stevenson <Ross.w.Stevenson@gmail.com>
2022-03-10 11:55:33 +0000

    updated styling to make the interface clearer and mobile-friendly

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import { v4 as uuidv4 } from 'uuid'
@@ -7,57 +7,61 @@ const LOCAL_STORAGE_KEY = 'todoApp.tools'
 function App() {
 
 
-const [todos, setTodos] = useState([])
-const todoNameRef = useRef();
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef();
 
-// on load, display any existing todo's (if there are any). Convert to array using JSON.parse
-useEffect(() => {
-  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-  if (storedTodos) setTodos(storedTodos)
-}, [])
+  // on load, display any existing todo's (if there are any). Convert to array using JSON.parse
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
 
-// save todos to local storage
-useEffect(() => {
-localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-}, [todos])
+  // save todos to local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
-// togle Todos from incomplete/complete
-function toggleTodo(id) {
-  const newTodos = [...todos]
-  const todo = newTodos.find(todo => todo.id === id)
-  todo.complete = ! todo.complete
-  setTodos(newTodos)
-}
+  // togle Todos from incomplete/complete
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
-function handleClearTodos(e) {
-  const newTodos =todos.filter(todo => !todo.complete)
-  setTodos(newTodos)
-}
+  function handleClearTodos(e) {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
 
-function handleAddTodo(e) {
-  // iD = iD + 1;
-  
-  const name = todoNameRef.current.value
-  if (name === '') return 
+  function handleAddTodo(e) {
+   
+    const name = todoNameRef.current.value
+    if (name === '') return
     setTodos(prevTodos => {
-      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
     }
-      )
+    )
     console.log(name)
     todoNameRef.current.value = null;
-  
-}
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-      
-      <h1> To do list</h1>
-      <TodoList todos={todos} toggleTodo={toggleTodo}/>
-      <input ref={todoNameRef} type = "text"/>
-      <button onClick = {handleAddTodo}>Add Todo</button>
-      <button onClick={handleClearTodos}>Clear list</button>
-      <div>you have {todos.filter(todo => !todo.complete).length} left to do </div>
 
+        <h1> To-do list </h1>
+        {/* <div className = "Test-Class2"> */}
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+        {/* </div> */}
+        
+        <input className = "Text-input" ref={todoNameRef} type="text" />
+        
+        <button className = "Add-button" onClick={handleAddTodo}>Add</button> 
+        <button className = "Add-button" onClick={handleClearTodos}>Clear done</button>
+        
+        {/* <div>you have {todos.filter(todo => !todo.complete).length} left to do </div> */}
+        
 
 
 
--- my-app/src/Todo.js
+++ my-app/src/Todo.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import './App.css';
 
 export default function Todo({ todo, toggleTodo }) {
     function handleTodoClick() {
         toggleTodo(todo.id)
     }
   return (
-    <div>
-        <label>
-        {todo.name} 
-        <input type="checkbox" checked={todo.complete} onChange={handleTodoClick}/>
+    <div className = "Test-Class">
+        <label >
         
+        <input className = "checkbox" type="checkbox" checked={todo.complete} onChange={handleTodoClick}/>
+        {todo.name} 
         </label>
         
         </div>
